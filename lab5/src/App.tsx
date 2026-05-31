56743bb2c1eb00665eb54c5122919d9432a40d54
labivi2 <labivi2.lolasr03@gmail.com>
2026-06-01 00:47:26 +0300

    Lab 5: Initial Vite + React + TypeScript setup

---
+++ lab5/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Лабораторна робота №5</h1>
+      <p>Інтеграція з API</p>
+    </main>
+  )
+}
+
+export default App
