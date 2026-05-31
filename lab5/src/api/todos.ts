486fdfa320e3621f43a24421d285acc0711811d3
labivi2 <labivi2.lolasr03@gmail.com>
2026-06-01 01:01:35 +0300

    Lab 5: Add Todo types and API service

---
+++ lab5/src/api/todos.ts
@@ -0,0 +1,45 @@
+﻿import type { CreateTodoDto, Todo, UpdateTodoDto } from '../types/todo'
+
+const API_URL = 'http://localhost:3001'
+
+async function request<T>(url: string, options?: RequestInit): Promise<T> {
+  const response = await fetch(`${API_URL}${url}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options?.headers,
+    },
+    ...options,
+  })
+
+  if (!response.ok) {
+    throw new Error('Помилка запиту до сервера')
+  }
+
+  return response.json()
+}
+
+export const todosApi = {
+  getAll: () => request<Todo[]>('/todos'),
+
+  create: (todo: CreateTodoDto) =>
+    request<Todo>('/todos', {
+      method: 'POST',
+      body: JSON.stringify(todo),
+    }),
+
+  update: (id: number, todo: UpdateTodoDto) =>
+    request<Todo>(`/todos/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(todo),
+    }),
+
+  remove: async (id: number) => {
+    const response = await fetch(`${API_URL}/todos/${id}`, {
+      method: 'DELETE',
+    })
+
+    if (!response.ok) {
+      throw new Error('Помилка видалення завдання')
+    }
+  },
+}
