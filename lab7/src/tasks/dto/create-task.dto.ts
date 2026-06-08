4f89eb3869b6a10dcb6da290b0a1928b1daaee4b
labivi2 <labivi2.lolasr03@gmail.com>
2026-06-08 03:20:39 +0300

    Lab 7: Fix

--- lab7/src/tasks/dto/create-task.dto.ts
+++ lab7/src/tasks/dto/create-task.dto.ts
@@ -10,7 +10,7 @@ export class CreateTaskDto {
   @IsString()
   @IsNotEmpty({ message: 'Назва не може бути порожньою' })
   @MaxLength(100)
-  title: string;
+  title!: string;
 
   @IsOptional()
   @IsString()
@@ -20,5 +20,5 @@ export class CreateTaskDto {
   @IsIn(['low', 'medium', 'high'], {
     message: 'Пріоритет має бути: low, medium або high',
   })
-  priority: 'low' | 'medium' | 'high';
+  priority!: 'low' | 'medium' | 'high';
 }
