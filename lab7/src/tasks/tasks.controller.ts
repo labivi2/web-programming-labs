c7ba26004491119c8b1195f418edac2dc8aa8bf3
labivi2 <labivi2.lolasr03@gmail.com>
2026-06-08 03:06:25 +0300

    Lab 7: Implement CRUD controller

--- lab7/src/tasks/tasks.controller.ts
+++ lab7/src/tasks/tasks.controller.ts
@@ -1,4 +1,69 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import type { Task } from './entities/task.entity';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  findAll(): Task[] {
+    return this.tasksService.findAll();
+  }
+
+  @Get('search')
+  findByStatus(@Query('status') status?: string): Task[] {
+    return this.tasksService.findByStatus(status);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Task {
+    const task = this.tasksService.findOne(id);
+
+    if (!task) {
+      throw new NotFoundException(`Завдання #${id} не знайдено`);
+    }
+
+    return task;
+  }
+
+  @Post()
+  @HttpCode(201)
+  create(@Body() dto: CreateTaskDto): Task {
+    return this.tasksService.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateTaskDto): Task {
+    const task = this.tasksService.update(id, dto);
+
+    if (!task) {
+      throw new NotFoundException(`Завдання #${id} не знайдено`);
+    }
+
+    return task;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: string): void {
+    const removed = this.tasksService.remove(id);
+
+    if (!removed) {
+      throw new NotFoundException(`Завдання #${id} не знайдено`);
+    }
+  }
+}
