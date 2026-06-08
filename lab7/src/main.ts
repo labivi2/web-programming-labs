f010e50c6d2e99849e3e6334fedc69202b4bd853
labivi2 <labivi2.lolasr03@gmail.com>
2026-06-08 03:01:19 +0300

    Lab 7: Add configuration and validation

--- lab7/src/main.ts
+++ lab7/src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  const port = parseInt(process.env.PORT ?? '3000', 10);
+  await app.listen(port);
+  console.log(`Server started on port ${port}`);
 }
 bootstrap();
